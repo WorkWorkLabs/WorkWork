a1892f96e577d53a61789ed44048b977ed5dcc62
yanboishere <yanbotravelaroundworld@gmail.com>
2025-12-28 07:26:50 +0800

    feat(auth): 添加 ParticleAuthWrapper 组件包裹子组件
    ci: 更新 Particle API 环境变量命名以支持客户端访问

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <TRPCProvider>{children}</TRPCProvider>
+        <TRPCProvider>
+          <ParticleAuthWrapper>{children}</ParticleAuthWrapper>
+        </TRPCProvider>
       </body>
     </html>
   );
